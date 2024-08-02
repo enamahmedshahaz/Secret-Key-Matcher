6308192cafc4a1a2d383d2d12224db45a3a311ab
enamahmedshahaz <enamahmedshahaz@gmail.com>
2024-08-02 20:02:48 +0600

    Implement function to show input password

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 let generatedPassword = '';
 
+let enteredPassword = '';
+
 function generateBtnHandler() {
     generatedPassword = getRandomPass();
     document.getElementById('pass-gen-field').setAttribute('value', generatedPassword);
@@ -10,3 +12,31 @@ function getRandomPass() {
     return pass;
 }
 
+
+function inputBtnHandler(event) {
+
+    let btnClassName = event.target.getAttribute('class');
+    let notNumButtonPressed = btnClassName.includes('btn-delete') || btnClassName.includes('btn-clear') || btnClassName.includes('btn-submit');
+
+    if (notNumButtonPressed) {
+        console.log('control button pressed');
+
+    } else {
+        let pressedNumber = event.target.innerText;
+        showNumberToInputField(pressedNumber);
+        console.log('Number button pressed: ', event.target.innerText);
+    }
+}
+
+function showNumberToInputField(pressedNumber) {
+
+    const inputField = document.getElementById('pass-input-field');
+    let prevNumber = inputField.getAttribute('value');
+
+    //Don't add more than six digit number to input field
+    if (prevNumber.length < 6) {
+        let newValue = prevNumber.concat(pressedNumber);
+        inputField.setAttribute('value', newValue);
+        enteredPassword = inputField.getAttribute('value');
+    }
+}
